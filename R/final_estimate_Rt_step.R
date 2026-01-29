1e51c9e07513ea97d1ddc69041890b2ccfa00e48
meaganjharrington <meaganjharrington@gmail.com>
2026-01-29 14:45:39 +0000

    add comments and documentation

--- R/final_estimate_Rt_step.R
+++ R/final_estimate_Rt_step.R
@@ -235,7 +235,7 @@ final_estimate_Rt_step <- function(
   mod <- monty::monty_model(list(density = density_theta, parameters = par_names)) # monty model object (w/ density and pars)(density = log posterior density to sample from)
   set.seed(seed) # fixed random number gen
   sampler <- monty::monty_sampler_random_walk(proposal) # cteaye rw MH sampler
-  n_burn  <- floor(burnin * n_steps) # compute burn-in no (burnin 0.5 = first 1/2 of samples dropped)(?)
+  n_burn  <- floor(burnin * n_steps) # compute burn-in no (burnin 0.5 = first 1/2 of samples dropped)(burn-in iterations as integer count)
   sample     <- monty::monty_sample(mod, sampler, n_steps, initial = init, n_chains = 1) # runs MCMC!
   # runs n_steps iterations, produces monty_samples w/ parameter values each time
 
